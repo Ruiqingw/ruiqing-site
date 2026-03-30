6882e3ef113a11f0810e75f08ce0550afce3315f
Ruiqing <ruiqingw2002@gmail.com>
2026-03-30 01:32:00 -0400

    note.sh: auto-detect changed filenames for commit message

--- note.sh
+++ note.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-git add src/data/notes.json
-git commit -m "${1:-update notes}"
+git add .
+# 自动用变动的文件名作为 commit message
+CHANGED=$(git diff --cached --name-only | xargs -I{} basename {} | tr '\n' ' ' | sed 's/ $//')
+MSG="${1:-update: $CHANGED}"
+git commit -m "$MSG"
 git push
